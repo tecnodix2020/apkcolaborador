eb3d149771a41798d1261f739433ac675c87ed7e
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-09-27 00:09:02 -0300

    Alterações para implementação da notificação

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React, { Component } from 'react';
+import React, { useEffect } from 'react'
+import { View, StyleSheet, Text, Button } from 'react-native'
+import { fcmService } from './services/FCMService'
+import { localNotificationService } from './services/LocalNotificationService'
 
-import Navigator from './routes/homeStack';
+export default function App() {
+  useEffect(() => {
+    fcmService.registerAppWithFCM()
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
 
-class App extends Component {
-    render() {
-      return (
-        <Navigator />
-      );
+    function onRegister(token) {
+      console.log("[App] onRegister: ", token)
     }
-};
 
-export default App;
+    function onNotification(notify) {
+      console.log("[App] onNotification: ", notify)
+      const options = {
+        soundName: 'default',
+        playSound: true
+      }
+      localNotificationService.showNotification(0, notify.title, notify.body, notify, options)
+    }
+
+    function onOpenNotification(notify) {
+      console.log("[App] onOpenNotification: ", notify)
+      alert("Open Notification: " + notify.body)
+    }
+
+    return () => {
+      console.log("[App] unRegister")
+      fcmService.unRegister()
+      localNotificationService.unRegister()
+    }
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text>APK Colaborador</Text>
+      <Button
+        title="Pressione o botão"
+        onPress={() => localNotificationService.cancelAllLocalNotifications()}  
+      />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
--- ios/SecretariaVirtualTotemTests/SecretariaVirtualTotemTests.m
+++ ios/SecretariaVirtualTotemTests/SecretariaVirtualTotemTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SecretariaVirtualTotemTests : XCTestCase
+@interface ApkColaboradorTests : XCTestCase
 
 @end
 
-@implementation SecretariaVirtualTotemTests
+@implementation ApkColaboradorTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
