b6e2c4a4556be27d8505b59ae231812d3b423ed8
Guilherme Ferreira <guifrribeiro@gmail.com>
2020-12-12 00:03:37 -0300

    Modifications to work notification and update gitignore

--- App.js
+++ App.js
@@ -5,16 +5,42 @@
  * @format
  * @flow strict-local
  */
-import React, { Component } from 'react';
-
+import React, { useEffect } from 'react';
+import { fcmService } from './services/FCMService';
+import { localNotificationService } from './services/LocalNotificationService';
 import Navigator from './routes/homeStack';
 
-class App extends Component {
-    render() {
-      return (
-          <Navigator />
-      );
-    }
-};
+export default function App() {
+  useEffect(() => {
+    fcmService.registerAppWithFCM()
+    fcmService.register(onRegister, onNotification, onOpenNotification)
+    localNotificationService.configure(onOpenNotification)
 
-export default App;
+    function onRegister(token) {
+      console.log("[App] onRegister: ", token)
+    }
+
+    function onNotification(notify) {
+      console.log("[App] onNotification: ", notify)
+      const options = {
+        soundName: 'default',
+        playSound: true
+      }
+      localNotificationService.showNotification(0, notify.title, notify.body, notify, options)          
+    }
+
+    function onOpenNotification(notify) {
+      console.log("[App] onOpenNotification: ", notify)
+      alert("Open Notification: " + notify.body)
+    }
+
+    return () => {
+      console.log("[App] unRegister")
+      fcmService.unRegister()
+      localNotificationService.unRegister()
+    }
+  }, []);
+  return (
+      <Navigator />
+  );
+}
