33638f6508532d5b802bd8f272f08d08d1755a70
Tecnodix2020 <tecnodix2020@gmail.com>
2020-12-13 18:14:53 -0300

    Ajustes realizados de layout e orientação alterada das mensagens

--- screens/chat.js
+++ screens/chat.js
@@ -14,13 +14,19 @@ import { Divider } from 'react-native-paper';
 
 import MessageBubble from '../components/messagebubble';
 
+import Message from '../components/messages';
+
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function Chat({ navigation }) {
 
   const [text, setTextName] = useState('');
+  const [textTotem, setTextTotem] = useState('');
   const [userLoggedIn, setUserLoggedIn] = useState('');
-
+  const [show, setShow] = useState(false);
+  const [valueResponse, setValueResponse] = useState('');
+  const [showResponse, setShowResponse] = useState(false);
+  const [showHist, setShowHist] = useState(false);
   const menuIcon = (<Icon name="bars" size={40} color="grey"/>)
 
   const openLeftMenu = () => {
@@ -37,6 +43,33 @@ export default function Chat({ navigation }) {
     getUserLoggedName();
     return () => { isMounted = false }; 
   });
+
+  const handleHistoricMsg = ()=> {
+    setTextTotem("Olá " + userLoggedIn + " - teste");
+    setShow(!show);
+    setShowHist(!showHist);
+  }
+
+  const handleMsg1 = () => { 
+    setShowResponse(true);
+    setValueResponse("Olá, já lhe encontro em alguns minutos");
+  }
+  const handleMsg2 = () => { 
+    setShowResponse(true);
+    setValueResponse("Olá, um de nossos colaboradores irá lhe recepcionar em alguns minutos");
+  }
+  const handleMsg3 = () => { 
+    setShowResponse(true);
+    setValueResponse("A equipe Gente foi acionada");
+  }
+
+  const send = () => {
+    console.log("Enviar a mensagem ao totem!");
+  }
+
+  const eyeIconShow = (<Icon name="eye" size={30} color="white"/>);
+  const eyeIconHide = (<Icon name="eye-slash" size={30} color="white"/>);
+  const robot = (<Icon name="envelope" size={40} color="lightgrey"/>);
   
   return (
     <View style={styles.body}>
@@ -53,15 +86,17 @@ export default function Chat({ navigation }) {
           cardMaxElevation={6}
           cornerRadius={9}>
 
-          <ScrollView>
+          <View style={styles.btnTop}>
+            <TouchableOpacity style={styles.submitContainer} onPress={handleHistoricMsg}>
+              <Text style={styles.submit}> Histórico </Text>
+              {!showHist && (<View>{eyeIconShow}</View>)}
+              {showHist && (<View>{eyeIconHide}</View>)}
+            </TouchableOpacity>
+          </View>
 
-            <MessageBubble
-              mine
-              text="Olá Guilherme, tenho uma encomenda para você."
-            />
-            <MessageBubble
-              text="Obrigado, estou indo até aí fazer a retirada."
-            />
+          <Divider/>
+
+          {show && (<ScrollView>
 
             <Divider/>
             
@@ -69,13 +104,10 @@ export default function Chat({ navigation }) {
               1 de Outubro 2020
             </Text> 
 
-            <MessageBubble
-              mine
-              text="Olá Guilherme, cheguei para nossa reunião."
-            />
-            <MessageBubble
-              text="Tudo bem, gentileza entre e aguarde nos bancos, já lhe encontro em alguns minutos."
-            />
+            
+            <Message code={1} owner={userLoggedIn} isMine={true}/>
+            {robot}
+            <Message code={5} isMine={false}/>
 
             <Divider/>
 
@@ -83,20 +115,41 @@ export default function Chat({ navigation }) {
               15 de Outubro 2020
             </Text> 
 
-            <MessageBubble
-              mine
-              text="Olá Guilherme, cheguei para nossa reunião."
-            />
+            <Message code={2} owner={userLoggedIn} isMine={true}/>
+            {robot}
+            <Message code={4} isMine={false}/>
 
-            <MessageBubble
-              mine
-              image={require('../img/opcaoentrega1.png')}
-            />
-
-            <MessageBubble
-              text={text}
-            />
           </ScrollView>
+          )}
+
+          {!show && (<View>
+          
+            <View>
+              <Text style={styles.txtMensagen}> Não Há Novas Mensagens </Text>
+            </View>
+            <View>
+              <Message code={1} owner={userLoggedIn} isMine={true}/>
+              {robot}
+              <TouchableOpacity style={styles.optionsContainer} onPress={handleMsg1}>
+                <Text style={styles.txtOptions}> 1 - Irá Receber </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionsContainer} onPress={handleMsg2}>
+                <Text style={styles.txtOptions}> 2 - Acionar Outro Colaborador </Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.optionsContainer} onPress={handleMsg3}>
+                <Text style={styles.txtOptions}> 3 - Acionar Equipe Gente </Text>
+              </TouchableOpacity>
+            </View>
+            
+            {showResponse && (
+              <View>
+                <MessageBubble text={valueResponse}/>
+                <TouchableOpacity style={styles.sendContainer} onPress={send}>
+                  <Text style={styles.txtSend}> ENVIAR RESPOSTA </Text>
+                </TouchableOpacity>
+              </View>)}
+          </View>
+          )}
 
         </CardView>
 
@@ -146,22 +199,89 @@ const styles = StyleSheet.create({
     backgroundColor: 'red',
   },
   text1: {
-    justifyContent: 'center',
+    alignContent: 'center',
     alignItems: 'center',
   },
   menu: {
     backgroundColor: '#fff',
     flexDirection: 'row',
     opacity: 0.8,
+    justifyContent: 'flex-end',
   },
   icon: {
-    marginLeft: wp(2),
+    marginRight: wp(2),
   },
   userText: {
-    marginLeft: wp(5),
-    alignContent: 'center',
+    marginRight: wp(30),
     marginTop: wp(2),
     color: 'black',
-    fontSize: wp(4.6),
+    fontSize: wp(5.1),
+  },
+  btnTop: {
+    alignItems: 'flex-end',
+    marginRight: wp(1),
+  },
+  submitContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginTop: wp(1),
+    marginBottom: wp(2),
+    width: '50%',
+    flexDirection: 'row',
+  },
+  submit: {
+    fontSize: wp(5.5),
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  txtMensagen: {
+    fontSize: wp(3.5),
+    marginTop: wp(1.5),
+    color: "blue",
+    alignSelf: "center",
+    textTransform: "uppercase"
+  },
+  optionsContainer: {
+    elevation: 8,
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginTop: wp(1),
+    marginBottom: wp(1),
+    marginLeft: wp(8),
+    width: '80%',
+    flexDirection: 'row',
+  },
+  txtOptions: {
+    fontSize: wp(3.5),
+    color: "white",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    fontWeight: "bold",
+  },
+  sendContainer: {
+    elevation: 8,
+    backgroundColor: "green",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginBottom: wp(1),
+    marginLeft: wp(60),
+    width: wp(20),
+    flexDirection: 'row',
+    alignSelf: "center",
+  },
+  txtSend: {
+    fontSize: wp(2.5),
+    color: "white",
+    alignSelf: "center",
+    textTransform: "uppercase",
+    fontWeight: "bold",
   }
 });
